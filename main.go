c4750319ba4322c4fc88b42f3beae3c7c89cad54
longkey1 <longkey1@gmail.com>
2026-03-03 08:47:50 +0900

    refactor: remove version initialization from main function
    - Remove unused version package import and variable assignments from main.go
    - Version information initialization is now handled elsewhere in the codebase
    - Simplify main function by removing version setup logic

--- main.go
+++ main.go
@@ -4,21 +4,9 @@ import (
 	"os"
 
 	"github.com/longkey1/gosla/cmd"
-	"github.com/longkey1/gosla/internal/version"
-)
-
-// Version information (set by ldflags)
-var (
-	ver    = "dev"
-	commit = "unknown"
-	date   = "unknown"
 )
 
 func main() {
-	version.Version = ver
-	version.CommitSHA = commit
-	version.BuildTime = date
-
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
 	}
